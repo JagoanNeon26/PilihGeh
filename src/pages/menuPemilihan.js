85e343b64e56be2f44b462aadc0c4992da92cb8c
Muhammad Bintang Pramadipta <pramadiptabintang26@gmail.com>
2023-07-10 21:17:00 +0700

    Add Text Area Form in Add Voting

--- src/pages/menuPemilihan.js
+++ src/pages/menuPemilihan.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from 'components/molecules/Navbar/navbar';
-import { Row, Col, Tabs, Tab } from 'react-bootstrap';
-import BaseButton from 'components/atoms/Button/button';
+import { Row, Col, Tabs, Tab, Button } from 'react-bootstrap';
 import DynamicTable from 'components/molecules/Table/DynamicTable';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import ModalAddVoting from 'components/molecules/Modal/modal';
 import styles from '../styles/Home.module.css';
 
-function menuPemilihan() {
+function MenuPemilihan() {
+  const [modalShow, setModalShow] = useState(false);
+  const handleShow = () => setModalShow(true);
+  const handleClose = () => setModalShow(false);
+
   const headersVoting = ['VOTING NAME', 'DATE', 'VOTE STATUS', 'VOTING STATUS'];
   const headersAdmin = ['VOTING NAME', 'DATE', '', 'VOTING STATUS'];
   const serverDataVoting = [
@@ -56,38 +60,42 @@ function menuPemilihan() {
   ]);
 
   return (
-    <div style={{ overflowX: 'hidden' }}>
-      <Navbar />
-      <Row>
-        <Col className={styles.judul}>
-          <h1>My Voting</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col className={styles.tabs}>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'flex-end',
-              marginBottom: '-30px',
-            }}
-          >
-            <BaseButton type="submit">
-              <div style={{ width: '60px' }}>Add</div>
-            </BaseButton>
-          </div>
-          <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
-            <Tab eventKey="home" title="Home">
-              <DynamicTable headers={headersVoting} data={dataVoting} />
-            </Tab>
-            <Tab eventKey="profile" title="Profile">
-              <DynamicTable headers={headersAdmin} data={dataAdmin} />
-            </Tab>
-          </Tabs>
-        </Col>
-      </Row>
-    </div>
+    <>
+      <div style={{ overflowX: 'hidden' }}>
+        <Navbar />
+        <Row>
+          <Col className={styles.judul}>
+            <h1>My Voting</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col className={styles.tabs}>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                marginBottom: '-30px',
+              }}
+            >
+              <Button onClick={handleShow}>
+                <div style={{ width: '60px' }}>Add</div>
+              </Button>
+            </div>
+            <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
+              <Tab eventKey="home" title="Home">
+                <DynamicTable headers={headersVoting} data={dataVoting} />
+              </Tab>
+              <Tab eventKey="profile" title="Profile">
+                <DynamicTable headers={headersAdmin} data={dataAdmin} />
+              </Tab>
+            </Tabs>
+          </Col>
+        </Row>
+      </div>
+
+      <ModalAddVoting show={modalShow} onHide={() => handleClose()} />
+    </>
   );
 }
 
-export default menuPemilihan;
+export default MenuPemilihan;
